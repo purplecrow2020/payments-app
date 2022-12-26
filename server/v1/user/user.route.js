8d89be40563b687c94d7b6fe4215f8d774e6bc01
akshat <akshat@akshats-MacBook-Pro.local>
2022-12-26 14:50:07 +0530

    :lock: made secure

--- server/v1/user/user.route.js
+++ server/v1/user/user.route.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const validate = require('express-validator');
 const paramValidation = require('./routeValidation');
+const AuthGaurd = require('../../../middlewares/auth');
 
 const userCtrl = require('./user.controller');
 const router = express.Router();
 
 router.route('/register').post(validate(paramValidation.user.signup), userCtrl.register);
 router.route('/get').get(validate(paramValidation.user.get), userCtrl.getUser);
-router.route('/delete').delete(validate(paramValidation.user.delete), userCtrl.deleteUser);
-router.route('/update').put(validate(paramValidation.user.update), userCtrl.updateUser);
+router.route('/delete').delete(AuthGaurd, validate(paramValidation.user.delete), userCtrl.deleteUser);
+router.route('/update').put(AuthGaurd, validate(paramValidation.user.update), userCtrl.updateUser);
 
 module.exports = router;
